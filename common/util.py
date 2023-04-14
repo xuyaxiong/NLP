5cd71206a21e07b985907d3f3797f8976bd69e8a
xu yaxiong <xyxlindy@163.com>
2023-04-14 20:44:25 +0800

    提交

--- common/util.py
+++ common/util.py
@@ -26,6 +26,63 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
     return np.array(contexts), np.array(target)
 
+
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / np.sqrt(np.sum(x ** 2) + eps)
+    ny = y / np.sqrt(np.sum(y ** 2) + eps)
+    return np.dot(nx, ny)
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    if query not in word_to_id:
+        print('%s is not found' % query)
+        return 
+    print('\n[query] ' + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(query_vec, word_matrix[i])
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if i == query_id:
+            continue
+        print('%s: %s' % (id_to_word[i], similarity[i]))
+        count += 1
+        if count >= top:
+            return
+        
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+def ppmi(C, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    row_num, col_num = C.shape
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    for i in range(row_num):
+        for j in range(col_num):
+            pmi = np.log2(C[i, j] * N / (S[i] * S[j]) + eps)
+            M[i, j] = max(0, pmi)
+    return M
+
 def convert_one_hot(corpus, vocab_size):
     N = corpus.shape[0]
     if corpus.ndim == 1:
@@ -40,3 +97,15 @@ def convert_one_hot(corpus, vocab_size):
                 one_hot[idx_0, idx_1, word_id] = 1
                 
     return one_hot
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
