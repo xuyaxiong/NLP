f971d09dfef45049989b0be94af3d3b67baecea8
xuyaxiong <xyxlindy@163.com>
2023-04-25 18:52:42 +0800

    添加EmbeddingDot层

--- common/layers.py
+++ common/layers.py
@@ -44,3 +44,46 @@ class SoftmaxWithLoss:
         dx *= dout
         dx = dx / batch_size
         return dx
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+        return None
+    
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+        self.cache = (target_W, h)
+        return out
+    
+    def backward(self, dout):
+        dout = dout.reshape(-1, 1)
+        target_W, h = self.cache
+        dtarget_w = dout * h
+        self.embed.backward(dtarget_w)
+        dh = dout * target_W
+        return dh
