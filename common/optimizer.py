5f3a90c71236ae65c9a36e776172b03221de9e3a
xuyaxiong <xyxlindy@163.com>
2023-04-27 11:19:18 +0800

    更新Adam

--- common/optimizer.py
+++ common/optimizer.py
@@ -1,30 +1,29 @@
 import numpy as np
 
 class Adam:
-
     def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
-        self.iter = 0
         self.m = None
         self.v = None
 
     def update(self, params, grads):
         if self.m is None:
-            self.m, self.v = [], []
+            self.m = []
+            self.v = []
             for param in params:
                 self.m.append(np.zeros_like(param))
                 self.v.append(np.zeros_like(param))
-
-        self.iter += 1
-        lr_t = self.lr * np.sqrt(1.0 - self.beta2 ** self.iter) / (1.0 - self.beta1 ** self.iter)
-
+                
         for i in range(len(params)):
-            self.m[i] += (1 - self.beta1) * (grads[i] - self.m[i])
-            self.v[i] += (1 - self.beta2) * (grads[i] ** 2 - self.v[i])
+            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * grads[i]
+            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * grads[i] ** 2
 
-            params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7)
+            self.m_hat = self.m[i] / (1 - self.beta1 ** (1+i))
+            self.v_hat = self.v[i] / (1 - self.beta2 ** (1+i))
+        
+            params[i] -= self.lr * self.m_hat / (np.sqrt(self.v_hat) + 1e-7)
 
 
 class Momentum:
